3ed2850a294a812d35c6923e8ff50c7ee4bf9654
Homervador <pierre.puss@gmail.com>
2017-09-11 14:12:32 +0200

    correctif du mock

--- src/app/game/game.ts
+++ src/app/game/game.ts
@@ -1,6 +1,8 @@
-import 'Kind' from '../kind/kind';
-import 'Editor' from '../editor/editor';
-import 'Platform' from '../platform/platform';
+import { Kind } from '../kind/kind';
+import { Editor } from '../editor/editor';
+import { Platform } from '../platform/platform';
+import { GAMES } from './mock-game';
+
 
 export class Game {
   id: number;
@@ -8,12 +10,13 @@ export class Game {
   raite: number;
   addDate: Date;
   price: number;
-  editor: Editor;
+  editor: String;
   kindList: Kind[];
   platformList: Platform[];
   quantity: number;
   description: String;
   jacket: String;
-  releaseDate: Date;
+  releaseDate: string;
   pegi: number;
 }
+
