9b646c9a401ddd505bdecc78b405cec79cf24aac
orlando1108 <erwan.raulo2015@campus-eni.fr>
2017-09-12 13:26:48 +0200

    add shop component

---
+++ src/app/Shop/shop.component.ts
@@ -0,0 +1,25 @@
+//import { User } from '../user';
+//import { UserService } from './user.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Game } from '../game/game';
+import { GameService } from './game.service';
+
+@Component({
+  selector: 'app-shop',
+  templateUrl: './shop.component.html',
+  styleUrls: ['./shop.component.css']
+})
+export class ShopComponent implements OnInit {
+
+  private games: Game[];
+//  private id: number;
+
+  constructor(private gameService: GameService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+  //  this.id = this.route.snapshot.params['id'];
+    this.games = this.gameService.getGames();
+  }
+
+}
