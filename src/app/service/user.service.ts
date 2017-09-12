ea7eaf78d574cd47a4283102141b1e9f64f511d9
nathanABR <nathan.abraham@outlook.fr>
2017-09-12 11:38:42 +0200

    resolve error by VTanchereau

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,11 +1,11 @@
 import { User } from '../model/user';
-import { USERLIST } from '../model/mock/mock-user';
+import { USERS } from '../model/mock/mock-user';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class UserService {
   constructor() { }
   getProfile(id: number): User {
-    return USERLIST.find(elem => elem.id == id);
+    return USERS.find(elem => elem.id == id);
   }
 }
