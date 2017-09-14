1a2d5dbe63c6d45e68b828522ecfee3d91d17b48
nathanABR <nathan.abraham@outlook.fr>
2017-09-14 13:31:12 +0200

    add angular firebase on project + conf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AppRouterModule } from './app-router.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -17,6 +20,15 @@ import { CartService } from './service/cart.service';
 import { HttpModule } from '@angular/http';
 import { GameDetailService } from './service/game-detail.service';
 
+export const firebaseConfig = {
+    apiKey: 'AIzaSyBEiqoujNEbGLpflGc0SvC9LwYkXsRvA4g',
+    authDomain: 'yeoldegameshoppe-69b8e.firebaseapp.com',
+    databaseURL: 'https://yeoldegameshoppe-69b8e.firebaseio.com',
+    projectId: 'yeoldegameshoppe-69b8e',
+    storageBucket: '',
+    messagingSenderId: '929433934926'
+};
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -35,6 +47,9 @@ import { GameDetailService } from './service/game-detail.service';
     AppRouterModule,
     HttpModule,
     LoginModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [
     UserService,
