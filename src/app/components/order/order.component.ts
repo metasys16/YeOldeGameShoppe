c5d65bfff86381b644684e173869d35818021d42
Tanchereau <victor.tanchereau@gmail.com>
2017-09-15 11:11:45 +0200

    Added the cancel functionality on the address editing

--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -4,6 +4,7 @@ import { Order } from '../../model/order';
 import { User } from '../../model/user';
 import { Cart } from '../../model/cart';
 import { Delivery } from '../../model/delivery';
+import { Address } from '../../model/address';
 
 import { USERS } from '../../model/mock/mock-user';
 
@@ -17,14 +18,16 @@ import { DeliveryService } from '../../service/delivery.service';
 })
 
 export class OrderComponent implements OnInit{
-  private order: Order;
-  private user: User;
-  private cart: Cart;
-  private deliveries: Delivery[];
-  private totalPrice: number;
-  private selectedDelivery: Delivery;
-  private editBillingAddress: Boolean;
-  private editDeliveryAddress: Boolean;
+  order: Order;
+  user: User;
+  cart: Cart;
+  deliveries: Delivery[];
+  totalPrice: number;
+  selectedDelivery: Delivery;
+  editBillingAddress: Boolean;
+  editDeliveryAddress: Boolean;
+  tempBillingAddress: Address;
+  tempDeliveryAddress: Address;
 
   constructor(private cartService: CartService,
               private deliveryService: DeliveryService) { }
@@ -44,6 +47,7 @@ export class OrderComponent implements OnInit{
 
     if (this.user.billingAddress == undefined){
       this.order.billingAddress = { addressNumber:"", street: "", complement:"", city: "", postalCode: "", country: ""};
+      this.tempBillingAddress = { addressNumber:"", street: "", complement:"", city: "", postalCode: "", country: ""};
       this.editBillingAddress = true;
     }else{
       this.order.billingAddress = this.user.billingAddress;
@@ -51,6 +55,7 @@ export class OrderComponent implements OnInit{
     }
     if (this.user.deliveryAddress == undefined){
       this.order.deliveryAddress = { addressNumber:"", street: "", complement:"", city: "", postalCode: "", country: ""};
+      this.tempDeliveryAddress = { addressNumber:"", street: "", complement:"", city: "", postalCode: "", country: ""};
       this.editDeliveryAddress = true;
     }else{
       this.order.deliveryAddress = this.user.deliveryAddress;
@@ -68,17 +73,40 @@ export class OrderComponent implements OnInit{
 
   saveBillingAddress(): void{
     this.editBillingAddress = false;
+    this.order.billingAddress = this.cloneAddress(this.order.billingAddress, this.tempBillingAddress);
   }
 
   editBillingAddressClick(): void{
     this.editBillingAddress = true;
+    this.tempBillingAddress = this.cloneAddress(this.tempBillingAddress, this.order.billingAddress);
   }
 
-  saveDeliveryAddress(): void{
+  cancelEditBillingAddress(): void{
     this.editBillingAddress = false;
   }
 
+  saveDeliveryAddress(): void{
+    this.editDeliveryAddress = false;
+    this.order.deliveryAddress = this.cloneAddress(this.order.deliveryAddress, this.tempDeliveryAddress);
+  }
+
   editDeliveryAddressClick(): void{
-    this.editBillingAddress = true;
+    this.editDeliveryAddress = true;
+    this.tempDeliveryAddress = this.cloneAddress(this.tempDeliveryAddress, this.order.deliveryAddress);
+  }
+
+  cancelDeliveryAddress(): void{
+    this.editDeliveryAddress = false;
+  }
+
+  private cloneAddress(target: Address, source: Address): Address{
+    target.addressNumber = source.addressNumber;
+    target.street = source.street;
+    target.complement = source.complement;
+    target.city = source.city;
+    target.postalCode = source.postalCode;
+    target.country = source.country;
+
+    return target;
   }
 }
